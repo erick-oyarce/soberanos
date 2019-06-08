837f757493ff997f4cce22167f5f547e92ae5554
ERICK OYARCE <erick.oyarce@glgroup.cl>
2019-06-08 18:47:24 -0400

    pagina web inicio

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php 
+
+define('APPS_CRAFT_RECIPIENT_EMAIL_ADDRESS', 'contactenos@byteservices.cl');
+define('APPS_CRAFT_MAIL_SUBJECT', 'Nuevo email de contacto.');
+define('APPS_CRAFT_SUCCESS_MESSAGE', 'Correo electrónico enviado correctamente!');
